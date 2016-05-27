330c0301a32157f165283d26af83e83bb0fa174e
Your Name <you@example.com>
2016-05-27 11:38:36 +0200

    ne pas oublier de faire app/console asset install maj layout

--- src/Afpa/OthelloGameBundle/Controller/GameController.php
+++ src/Afpa/OthelloGameBundle/Controller/GameController.php
@@ -2,10 +2,19 @@
 
 namespace Afpa\OthelloGameBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class GameController extends Controller
-{
+class GameController extends Controller {
+
+    /**
+     * @Route("/", name="home")
+     * @Template()
+     */
+    public function homeAction() {
+        return array();
+    }
+
 }
