caee5322ad978d1c9eacbfba6b0bef62ec96d5bb
teamraider <teamraider13@gmail.com>
2016-05-30 16:07:34 +0200

    maj js modifié

--- src/Afpa/OthelloGameBundle/Resources/public/js/functions.js
+++ src/Afpa/OthelloGameBundle/Resources/public/js/functions.js
@@ -1,5 +1,4 @@
 $(function () {
-    alert('rrr');
     //Executer l'effet
     function runEffect() {
         $("#effect").show('scale', null, 500);
@@ -8,6 +7,24 @@ $(function () {
 
     $("#effect").hide();
     //setTimeout(runEffect, 2000);
+    
+    
+    $('#board td').click(function() {
+        //$(this).data(data-x);
+        console.log($(this).data('x') + ' ' + $(this).data('y'));
+        
+        // test : est-ce que la case est vide
+        // si oui, appel AJAX
+        if ($(this).html().indexOf("img") >= 0){
+          console.log('case occupée');  
+        }
+            
+        else {
+           console.log('case libre'); 
+        }
+            
+    });
 });
+
 
 
