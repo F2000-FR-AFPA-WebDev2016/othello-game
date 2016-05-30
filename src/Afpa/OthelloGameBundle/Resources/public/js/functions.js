bc7311694579b123ee17811537d5cd6ffafa24ad
Jenny13800 <skullinfire@aol.com>
2016-05-30 13:15:55 +0200

    Modif GameController / CSS / ViewEndGame/Layout + creation js functions

---
+++ src/Afpa/OthelloGameBundle/Resources/public/js/functions.js
@@ -0,0 +1,13 @@
+$(function () {
+    alert('rrr');
+    //Executer l'effet
+    function runEffect() {
+        $("#effect").show('scale', null, 500);
+    }
+
+
+    $("#effect").hide();
+    //setTimeout(runEffect, 2000);
+});
+
+
